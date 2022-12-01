f3a4ab84e5c0f9e9e5bb1880c88e9512c97e8d1a
NoahWest <noahwestberry@gmail.com>
2022-11-30 21:45:37 -0800

    set up initial routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./index.css";
+import Home from "./routes/Home"
+import Project from "./routes/Project"
+import About from "./routes/About"
+import Contact from "./routes/Contact"
 
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/Contact" element={<Contact />} />
+    </Routes>
+    </>
   );
 }
 
