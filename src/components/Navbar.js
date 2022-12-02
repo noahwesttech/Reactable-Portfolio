a2e0b0821769bf94073a16cf3ef098577e3d678a
NoahWest <noahwestberry@gmail.com>
2022-12-02 12:32:36 -0800

    fixed mobile functionality for hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
         <Link to="/">
             <h1>Portfolio</h1>
         </Link>
-        <ul className="nav-menu">
+        <ul className={ click ? "nav-menu active" : "nav-menu" }>
             <li>
                 <Link to="/">Home</Link>
             </li>
